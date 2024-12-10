36e2b04aa8bfc3f4635dd92ca9d6063eca30c0bc
Santiago <kirayandrade04@gmail.com>
2024-12-10 10:12:37 -0800

    turno

--- app/Models/turno.php
+++ app/Models/turno.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class turno extends Model
+class Turno extends Model
 {
     /** @use HasFactory<\Database\Factories\TurnoFactory> */
     use HasFactory;
